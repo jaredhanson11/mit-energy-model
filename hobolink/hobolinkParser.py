29170b8ba2eea264ab3ab201c2a1ecf26e2595cf
spencerkim99 <spencerkim@dhcp-18-189-104-130.dyn.MIT.EDU>
2017-10-02 20:54:55 -0400

    updated hobolinkParser

--- hobolink/hobolinkParser.py
+++ hobolink/hobolinkParser.py
@@ -12,33 +12,44 @@ input_df = pd.read_csv(abs_input_path)
 
 d = {'temperature':7, 'dew point': 9, 'rh':8, 'pressure':5, 'radiation':4, 'wind':6}
 ordering = ['year', 'month', 'day', 'hour', 'minute', 'temperature', 'dew point', 'rh', 'pressure', 'radiation', 'wind']
-results = {}
+io_mapping = {
+    7 : 6,  #temperature
+    9 : 7,  #dew point
+    8 : 8,  #rh
+    5 : 9,  #pressure
+    6 : 20  #wind direction
+}
 
-# for i in d.keys():
-#     col = input_df[input_df.columns[d[i]]].map(lambda x: x.replace(',', '') if type(x) is str else x)
-#     results[i] = col.astype(float).mean()
+NUM_EPW_INPUTS = 35
+NULL_EPW_VALUE = ''
 
 for index, row in input_df.iterrows():
-    date = datetime.strptime(row[1], '%m/%d/%y %H:%M:%S')
-    results['year'] = date.year
-    results['month'] = date.month
-    results['day'] = date.day
-    results['hour'] = date.hour
-    results['minute'] = date.minute
 
-    for i in d.keys():
-        results[i] = row[d[i]].replace(',', '') if type(row[d[i]]) is str else row[d[i]]
+    results = [NULL_EPW_VALUE for i in range(NUM_EPW_INPUTS)]
+
+    date = datetime.strptime(row[1], '%m/%d/%y %H:%M:%S')
+
+    results[0] = date.year
+    results[1] = date.month
+    results[2] = date.day
+    results[3] = date.hour
+    results[4] = date.minute
+
+    for input_col in io_mapping:
+
+        output_col = io_mapping[input_col]
+
+        if input_col == 5:
+            converted_value = row[io_mapping[input_col]] * 100
+            results[output_col] = converted_value
+            print (converted_value)
+
+        else:
+            results[output_col] = row[input_col].replace(',', '') if type(row[input_col]) is str else row[input_col]
+
+    results = map(lambda x: str(x), results)
+    csv_output_line = ",".join(results)
 
     csv = open(abs_output_path, "a")
-    output_str = ''
-    for i in ordering:
-        if i == 'minute':
-            output_str = output_str + ','
-        if i == 'wind':
-            output_str = output_str + ',,,,,,,,,'
 
-        output_str = output_str + str(results[i]) + ','
-    output_str = output_str[:-1]
-    csv.write(output_str + '\n')
-    print(results)
-
+    csv.write(csv_output_line + '\n')
