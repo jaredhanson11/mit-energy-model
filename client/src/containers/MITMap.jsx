247540587742610b24dc5d41cdb07f6030287799
nikhilwbhatia <nikhilbhatia@Nikhils-MacBook-Pro-2.local>
2017-10-23 22:35:31 -0400

    added correct geojson, added dummy colors

--- client/src/containers/MITMap.jsx
+++ client/src/containers/MITMap.jsx
@@ -1,59 +1,83 @@
 import React from 'react';
 
 import Building from './Building.jsx';
-import {Map, TileLayer, GeoJSON} from 'react-leaflet';
-const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
-const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+import {Map, TileLayer, GeoJSON, Marker, Popup} from 'react-leaflet';
 const center = [42.362432, -71.086086];
 const zoom = 14
 var GJV = require("geojson-validation");
 
-import campus from './project.json';
+import campus from './data.json';
 
-GJV.isFeature(campus, function(valid, errs){
-    //log the errors
-    if(!valid){
-       console.log(errs);
-    }
-});
-// var states = [{
-//     "type": "Feature",
-//     "properties": {"party": "Republican"},
-//     "geometry": {
-//         "type": "Polygon",
-//         "coordinates": [[
-//             [42.360971, -71.088065],
-//             [42.361185, -71.087386],
-//             [42.360380, -71.086885],
-//             [42.360170, -71.087561]
-//         ]]
+// GJV.isFeature(campus, function(valid, errs){
+//     //log the errors
+//     if(!valid){
+//        console.log(errs);
 //     }
-// }, {
-//     "type": "Feature",
-//     "properties": {"party": "Democrat"},
-//     "geometry": {
-//         "type": "Polygon",
-//         "coordinates": [[
-//             [-109.05, 41.00],
-//             [-102.06, 40.99],
-//             [-102.03, 36.99],
-//             [-109.04, 36.99],
-//             [-109.05, 41.00]
-//         ]]
-//     }
-// }];
+// });
 
 class MITMap extends React.Component {
+    style(feature) {
+        console.log(feature.properties.building_number)
+        if ("W" == feature.properties.building_number.charAt(0)) {
+            return {
+               weight: 2,
+               opacity: 1,
+               color: '#4b4b4b',
+               dashArray: '3',
+               fillOpacity: 0.5,
+               fillColor: '#ff0000'
+            };
+        }
+        else if ("E" == feature.properties.building_number.charAt(0)){
+            return {
+                weight: 2,
+                opacity: 1,
+                color: '#4b4b4b',
+                dashArray: '3',
+                fillOpacity: 0.5,
+                fillColor: '#f9fb0a'
+            };
+        }
+        else if ("N" == feature.properties.building_number.charAt(0)){
+            return {
+                weight: 2,
+                opacity: 1,
+                color: '#4b4b4b',
+                dashArray: '3',
+                fillOpacity: 0.5,
+                fillColor: '#01ff1f'
+            };
+        }
+        else {
+            return {
+               weight: 2,
+               opacity: 1,
+               color: '#4b4b4b',
+               dashArray: '3',
+               fillOpacity: 0.5,
+               fillColor: '#ff8100'
+            };
+         }
+    }
     render() {
         return (
             <div style={{marginTop:'20px'}}>
                 <Map
                     center={center}
-                    zoom={zoom} >
+                    zoom={zoom}>
                     <TileLayer
                         attribution='© OpenStreetMap'
                         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                     />
+
+                    <Marker position={[42.362432, -71.086086]}>
+                        <Popup>
+                            <div>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </div>
+                        </Popup>
+                    </Marker>
+                    <GeoJSON data={campus} style={this.style}/>
                 </Map>
             </div>
         )
