4b10664a5cd3cc41ca57ab368812ac544bdc6805
Jared Hanson <hansonj@mit.edu>
2017-11-13 23:59:33 -0500

    Supports redux devtools

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -2,13 +2,14 @@ import { applyMiddleware, createStore } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import { createLogger } from 'redux-logger';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = applyMiddleware(apiMiddleware, createLogger());
 
 var configureStore = function(reducer, initialState){
     return createStore(
         reducer,
         initialState,
-        middleware
+        composeEnhancers(middleware)
     );
 };
 
