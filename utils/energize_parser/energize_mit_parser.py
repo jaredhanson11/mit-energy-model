ac96fb6d9be4208a349337d9d3bb3b39f3075d4b
spencerkim99 <spencerkim@dhcp-18-189-104-130.dyn.MIT.EDU>
2017-11-14 01:12:40 -0500

    added compose

--- utils/energize_parser/energize_mit_parser.py
+++ utils/energize_parser/energize_mit_parser.py
@@ -24,7 +24,20 @@ with open('data_2017.csv', 'r') as f:
                 month = ''.join([line[42][5], line[42][6]])
                 building_data[line[0]][line[33]][month] = line[35]
 
+# months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+# null_buildings = []
+
+# for key, value in building_data.items():
+# 	for k, v in value.items():
+# 		new_list = []
+# 		for month in months:
+# 			if month in v:
+# 				new_list.append(v[month])
+# 			if month not in v:
+# 				new_list
+# 		value[k] = new_list
+
 print (json.dumps(building_data, indent=4))
 
-with open('data_2017.json', 'w') as outfile:
-    json.dump(building_data, outfile)
+# with open('data_2017.json', 'w') as outfile:
+#     json.dump(building_data, outfile)
